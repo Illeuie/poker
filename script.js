09fb0c8fd207293deb3332805fee727fff1023bc
Illeuie <D_imo_N@bigmir.net>
2022-10-06 10:10:46 +0300

    first  commit

---
+++ script.js
@@ -0,0 +1,564 @@
+	
+let card = [];
+
+				card[0] = {
+					name: "A",
+					cost: 11,
+					img: "img/Ap.jpg",
+				};
+				card[1]	= {
+					name: "A",
+					cost: 11,
+					img: "img/Ab.jpg",
+				};
+				card[2] = {
+					name: "A",
+					cost: 11,
+					img: "img/Ac.jpg",
+				};
+				card[3]	= {
+					name: "A",
+					cost: 11,
+					img: "img/Ax.jpg",
+				};
+
+
+				card[4] = {
+					name: "K",
+					cost: 10,
+					img: "img/Kp.jpg",
+				};
+				card[5]	= {
+					name: "K",
+					cost: 10,
+					img: "img/Kb.jpg",
+				};
+				card[6] = {
+					name: "K",
+					cost: 10,
+					img: "img/Kc.jpg",
+				};
+				card[7]	= {
+					name: "K",
+					cost: 10,
+					img: 'img/Kx.jpg',
+				};
+
+
+				card[8] = {
+					name: "D",
+					cost: 10,
+					img: "img/Dp.jpg",
+				};
+				card[9]	= {
+					name: "D",
+					cost: 10,
+					img: "img/Db.jpg",
+				};
+				card[10] = {
+					name: "D",
+					cost: 10,
+					img: "img/Dc.jpg",
+				};
+				card[11]	= {
+					name: "D",
+					cost: 10,
+					img: "img/Dx.jpg",
+				};
+
+
+				card[12] = {
+					name: "V",
+					cost: 10,
+					img: "img/Vp.jpg",
+				};
+				card[13]	= {
+					name: "V",
+					cost: 10,
+					img: "img/Vb.jpg",
+				};
+				card[14] = {
+					name: "V",
+					cost: 10,
+					img: "img/Vc.jpg",
+				};
+				card[15]	= {
+					name: "V",
+					cost: 10,
+					img: 'img/Vx.jpg',
+				};
+
+
+				card[16] = {
+					name: "10",
+					cost: 10,
+					img: "img/10p.jpg",
+				};
+				card[17]	= {
+					name: "10",
+					cost: 10,
+					img: "img/10b.jpg",
+				};
+				card[18] = {
+					name: "10",
+					cost: 10,
+					img: "img/10c.jpg",
+				};
+				card[19] = {
+					name: "10",
+					cost: 10,
+					img: "img/10x.jpg",
+				};
+
+
+				card[20] = {
+					name: "9",
+					cost: 9,
+					img: "img/9p.jpg",
+				};
+				card[21]	= {
+					name: "9",
+					cost: 9,
+					img: "img/9b.jpg",
+				};
+				card[22] = {
+					name: "9",
+					cost: 9,
+					img: "img/9c.jpg",
+				};
+				card[23]	= {
+					name: "9",
+					cost: 9,
+					img: 'img/9x.jpg',
+				};
+
+
+				card[24] = {
+					name: "8",
+					cost: 8,
+					img: "img/8p.jpg",
+				};
+				card[25]	= {
+					name: "8",
+					cost: 8,
+					img: "img/8b.jpg",
+				};
+				card[26] = {
+					name: "8",
+					cost: 8,
+					img: "img/8c.jpg",
+				};
+				card[27]	= {
+					name: "8",
+					cost: 8,
+					img: "img/8x.jpg",
+				};
+
+
+				card[28] = {
+					name: "7",
+					cost: 7,
+					img: "img/7p.jpg",
+				};
+				card[29]	= {
+					name: "7",
+					cost: 7,
+					img: "img/7b.jpg",
+				};
+				card[30] = {
+					name: "7",
+					cost: 7,
+					img: "img/7c.jpg",
+				};
+				card[31]	= {
+					name: "7",
+					cost: 7,
+					img: 'img/7x.jpg',
+				};
+
+
+				card[32] = {
+					name: "6",
+					cost: 6,
+					img: "img/6p.jpg",
+				};
+				card[33]	= {
+					name: "6",
+					cost: 6,
+					img: "img/6b.jpg",
+				};
+				card[34] = {
+					name: "6",
+					cost: 6,
+					img: "img/6c.jpg",
+				};
+				card[35]	= {
+					name: "6",
+					cost: 6,
+					img: "img/6x.jpg",
+				};
+
+
+				card[36] = {
+					name: "5",
+					cost: 5,
+					img: "img/5p.jpg",
+				};
+				card[37]	= {
+					name: "5",
+					cost: 5,
+					img: 'img/5b.jpg',
+				};
+				card[38] = {
+					name: "5",
+					cost: 5,
+					img: "img/5c.jpg",
+				};
+				card[39]	= {
+					name: "5",
+					cost: 5,
+					img: 'img/5x.jpg',
+				};
+
+
+				card[40] = {
+					name: "4",
+					cost: 4,
+					img: "img/4p.jpg",
+				};
+				card[41]	= {
+					name: "4",
+					cost: 4,
+					img: "img/4b.jpg",
+				};
+				card[42] = {
+					name: "4",
+					cost: 4,
+					img: "img/4c.jpg",
+				};
+				card[43]	= {
+					name: "4",
+					cost: 4,
+					img: "img/4x.jpg",
+				};
+
+
+				card[44] = {
+					name: "3",
+					cost: 3,
+					img: "img/3p.jpg",
+				};
+				card[45]	= {
+					name: "3",
+					cost: 3,
+					img: "img/3b.jpg",
+				};
+				card[46] = {
+					name: "3",
+					cost: 3,
+					img: "img/3c.jpg",
+				};
+				card[47]	= {
+					name: "3",
+					cost: 3,
+					img: 'img/3x.jpg',
+				};
+
+
+				card[48] = {
+					name: "2",
+					cost: 2,
+					img: "img/2p.jpg",
+				};
+				card[49]	= {
+					name: "2",
+					cost: 2,
+					img: "img/2b.jpg",
+				};
+				card[50] = {
+					name: "2",
+					cost: 2,
+					img: "img/2c.jpg",
+				};
+				card[51]	= {
+					name: "2",
+					cost: 2,
+					img: 'img/2x.jpg',
+				};
+
+		
+	 
+	const InvitedCards = document.getElementById(`cards`);
+	const InvitedCardsTwo = document.getElementById(`cardsTwo`);
+	const ButtonChange = document.querySelector(`.ButtonChange`);
+	const ButtonChangeTwo = document.querySelector(`.ButtonChangeTwo`);
+	const buttonNewGames = document.querySelector(`.buttonNewGames`);
+	const OllButton = document.querySelector(`.OllButton`);
+	const NewGamesPlayers = document.querySelector(`.NewGames`);
+	const NameCards = document.querySelector(`.yourСhoice`);
+	const yourСhoiceTwo = document.querySelector(`.yourСhoiceTwo`);
+	const cardsListTwo = document.getElementById(`cardsListTwo`);
+	const PlayerOldCardsList = document.querySelector(`.PlayerOldCardsList`);
+	const cardsList = document.getElementById("cardsList");
+	let cardsOne = document.querySelector(`.cardsOne`);
+	let cardsTwo = document.querySelector(`.cardsTwo`);
+	 let CheckBankOld = document.querySelector(`.CheckBank`);
+
+	 
+
+//	let r1 =document.querySelector(`.winFirst`); 
+	
+	
+
+
+	let cardOut2 = [];
+
+	 let inputTwo = 0;
+	 let inputOne = 0;
+	 let carCount= 0;
+	 let carCountTwo= 0;
+	 let counterVal = 0;
+	 let counterValTwo = 0;
+	 let cardRand = 0;
+	 let delI = 0;
+	 let CheckBank = 0;
+	 let inpOne = 0; 
+	  let inptwo = 0;
+
+
+
+	    function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+
+
+				function ButtonCards(){	
+	
+	let check = card[cardRand].cost;
+	let name = card[cardRand].name;	
+			InvitedCards.src  = card[cardRand].img;
+	//		cardsOne.src  = card[cardRand].img;
+			 cardOut = card.splice(cardRand, 1);
+			 carCount = carCount+check;
+			update(++counterVal);
+
+		if (carCount>21) {
+			ButtonChange.innerHTML = "перебор"
+			counterVal= 5;
+		}		
+		};	
+
+	function update() {
+    if (counterVal <= 5) {
+  
+			 NameCards.innerHTML = carCount;
+			let divImg =document.createElement(`img`);
+			divImg.className = "PlayerCardsOne";
+			divImg.src = cardOut[0].img;;
+			cardsList.appendChild(divImg);
+			cardOut2.push(cardOut);
+			
+				
+
+    }else{
+    	ButtonChange.innerHTML = "перебор карт";
+    	InvitedCards.src  = "img/385218.png";
+    	
+
+    }
+}
+
+			function ButtonCardsTwo(){	
+	
+	let check = card[cardRand].cost;
+	let name = card[cardRand].name;	
+			InvitedCards.src  = card[cardRand].img;
+	//		cardsOne.src  = card[cardRand].img;
+			 cardOut = card.splice(cardRand, 1);
+			 cardOut2.push(cardOut);
+			 	
+			 carCountTwo = carCountTwo+check;
+			updateTwo(++counterValTwo);
+		if (carCountTwo > 21) {
+			ButtonChangeTwo.innerHTML = "перебор"
+			counterValTwo = 5;
+		}
+					
+		};	
+		
+	function updateTwo() {
+    if (counterValTwo<=5) {
+    		
+    	
+			 yourСhoiceTwo.innerHTML = carCountTwo;
+			let divImg =document.createElement(`img`);
+			divImg.className = "PlayerCardsTwo";
+			divImg.src = cardOut[0].img;
+			
+			cardsListTwo.appendChild(divImg);
+
+			
+			
+
+    }else{
+    	ButtonChangeTwo.innerHTML = "перебор карт";
+    	InvitedCards.src  = "img/385218.png";
+    }
+}
+
+   function del() {
+//    document.getElementById('cardsList').innerHTML = "";
+		counterVal= 5;
+      return;
+    }
+    document.querySelector('.ResetButton').addEventListener('click', del);
+
+    document.querySelector('.ButtonChange').addEventListener('click', ButtonCards);
+ 
+    
+
+    
+
+ function delTwo() {
+  //    document.getElementById('cardsListTwo').innerHTML = "";
+        counterValTwo = 5;
+      return;
+    }
+
+    //События при нажатии на кнопки
+      document.querySelector('.ResetButtonTwo').addEventListener('click', delTwo);
+
+
+  	document.querySelector('.ButtonChangeTwo').addEventListener('click', ButtonCardsTwo);
+
+
+    function newGame(){
+    		shuffle(card);
+    	
+    	Nullification();
+    		if (card[0].name === card[2].name ) {
+    			// другий гравець перевірка мисиву 
+    			console.log("dkslfkd")
+    		}
+    		if (card[1].name === card[3].name ) {
+    			// перший гравець перевірка мисиву 
+    			console.log("5555")
+    		}
+    	for (let i = 0; i <= 1; i++) {
+    	ButtonCardsTwo();
+    	ButtonCards();
+    	OllButton.style.display = "block";
+    	document.querySelector('.buttonNewGames').style.display = "none";
+    	document.querySelector('.endGames').style.display = "";
+
+    } 
+    	
+   		 }
+     document.querySelector('.buttonNewGames').addEventListener('click', newGame);
+
+  
+
+    	function endGames(){
+	 	document.getElementById('cardsListTwo').innerHTML = "";
+	 	document.getElementById('cardsList').innerHTML = "";
+	 	ButtonChangeTwo.innerHTML = "Граємо";
+	 	ButtonChange.innerHTML = "Граємо";
+	 	OllButton.style.display = "none";
+    	buttonNewGames.style.display = "block";
+    	document.querySelector('.endGames').style.display = "none";
+
+    	if (carCount <=21 && carCount >carCountTwo) {
+    		
+    			WinFirstPlayer();
+			 
+    	}else if (carCount >21 && carCountTwo >21) {
+    		LoosPlayer();
+    		
+    			
+    	}else if(carCount === 0 && carCountTwo === 0) {
+    		console.log("лашня");
+    		
+    	}else if(carCountTwo >21){
+    		WinFirstPlayer();
+    			
+    	}else if(carCount===carCountTwo){
+    		  console.log("лашня");
+    		
+    	}else{
+    		  WinTwoPlayer();
+    		
+    	}
+
+    	}
+
+    document.querySelector('.endGames').addEventListener('click', endGames);
+
+    function Nullification(){
+    			carCount= 0;
+				carCountTwo= 0;
+		 		counterVal = 0;
+		 		counterValTwo = 0;
+		 		document.querySelector(`.winFirst`).innerHTML = "win";
+    		document.querySelector(`.winTwoWin`).innerHTML = "win";
+    }
+    function WinFirstPlayer(){
+    	let inpOne =  document.getElementById(`inp`).value;
+    	console.log(inpOne)
+    	let inpTwo =  document.getElementById(`inpTwo`).value;
+		//	let inputOne = inpOne + inpTwo;
+		//	document.getElementById(`inp`).value = inputOne;
+			let startingCapital = document.getElementById(`inpStartingCapital`).value;
+			let startingCapitalTwo = document.getElementById(`inpStartingCapitalTwo`).value;
+			let inputOne = Math.ceil(Number(startingCapital)) - Math.ceil(Number(inpOne));
+    		let inputOneOll = Math.ceil(Number(startingCapital)) + Math.ceil(Number(inpTwo));
+    		document.getElementById(`inpStartingCapital`).value = inputOneOll; 
+    		document.getElementById(`inpStartingCapitalTwo`).value = inputOne;
+    				
+			 document.querySelector(`.winFirst`).innerHTML = "You win";
+			  document.querySelector(`.winFirst`).style.color = "red";
+			  document.querySelector(`.winTwoWin`).innerHTML = "You Loos";
+    		  document.querySelector(`.winTwoWin`).style.color = "blue";
+    }
+    	function LoosPlayer(){
+    			let inpOne =  document.getElementById(`inp`).value;
+    			let inpTwo =  document.getElementById(`inpTwo`).value;
+    			console.log(inpOne)			
+			let startingCapital = document.getElementById(`inpStartingCapital`).value;
+    		let inputOneOll = Math.ceil(Number(startingCapital)) - Math.ceil(Number(inpOne));
+    		document.getElementById(`inpStartingCapital`).value = inputOneOll; 	
+			 document.querySelector(`.winFirst`).innerHTML = "You Loos";
+			  document.querySelector(`.winFirst`).style.color = "blue";
+			
+			   console.log(CheckBankOld)	
+			 CheckBank = Math.ceil(Number(inpOne)) + Math.ceil(Number(CheckBank));
+			CheckBankOld.innerHTML = CheckBank;
+			  console.log(CheckBank)	
+    	}
+    
+    function WinTwoPlayer(){
+    	let inpTwo =  document.getElementById(`inpTwo`).value;
+    	let inpOne =  document.getElementById(`inp`).value;
+		//	let inputTwo = Math.ceil(Number(inpOne)) + Math.ceil(Number(inpTwo))	
+			
+		//	document.getElementById(`inp`).value = inputOne;
+			let startingCapital = document.getElementById(`inpStartingCapital`).value;
+
+			let startingCapitalTwo = document.getElementById(`inpStartingCapitalTwo`).value;
+			let inputOne = Math.ceil(Number(startingCapital)) - Math.ceil(Number(inpTwo));
+    		let inputOneOll = Math.ceil(Number(startingCapitalTwo)) + Math.ceil(Number(inpOne));
+    		document.getElementById(`inpStartingCapitalTwo`).value = inputOneOll; 
+    		document.getElementById(`inpStartingCapital`).value = inputOne;
+    				
+			 document.querySelector(`.winFirst`).innerHTML = "You Loos";
+			  document.querySelector(`.winFirst`).style.color = "blue";
+			  document.querySelector(`.winTwoWin`).innerHTML = "You win";
+    		  document.querySelector(`.winTwoWin`).style.color = "red";
+    }
+    	function split(){
+    			
+    	}
